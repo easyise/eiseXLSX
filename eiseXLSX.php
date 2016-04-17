2f226ae414a7127735bbc0ad5108063b9f0c53f6
Ilya Eliseev <ie@e-ise.com>
2016-04-17 13:16:10 +0300

    integrated with convertDateTime(), added some docs
    Signed-off-by: Ilya Eliseev <ie@e-ise.com>

--- eiseXLSX.php
+++ eiseXLSX.php
@@ -864,56 +864,77 @@ private function addSharedString(&$oCell){
     return $oSharedString;
 }
 
-// Convert datetime to Excel format, thanks to Excel::Writer::XLSX::Worksheet.pm (perl)
-private function convertDateTime($date_input) {
-  $days    = 0;    # Number of days since epoch
-  $seconds = 0;    # Time expressed as fraction of 24h hours in seconds
-  $year=$month=$day=0;
-  $hour=$min  =$sec=0;
-  $date_time = $date_input;
-  if (preg_match("/(\d{4})\-(\d{2})\-(\d{2})/", $date_time, $matches))
-  {
-    list($junk,$year,$month,$day) = $matches;
-  }
-  if (preg_match("/(\d{2}):(\d{2}):(\d{2})/", $date_time, $matches))
-  {
-    list($junk,$hour,$min,$sec) = $matches;
-    $seconds = ( $hour * 60 * 60 + $min * 60 + $sec ) / ( 24 * 60 * 60 );
-  }
-  //using 1900 as epoch, not 1904, ignoring 1904 special case
-  # Special cases for Excel.
-  if ("$year-$month-$day"=='1899-12-31')  return $seconds      ;    # Excel 1900 epoch
-  if ("$year-$month-$day"=='1900-01-00')  return $seconds      ;    # Excel 1900 epoch
-  if ("$year-$month-$day"=='1900-02-29')  return 60 + $seconds ;    # Excel false leapday
-  # We calculate the date by calculating the number of days since the epoch
-  # and adjust for the number of leap days. We calculate the number of leap
-  # days by normalising the year in relation to the epoch. Thus the year 2000
-  # becomes 100 for 4 and 100 year leapdays and 400 for 400 year leapdays.
-  $epoch  = 1900;
-  $offset = 0;
-  $norm   = 300;
-  $range  = $year - $epoch;
-  # Set month days and check for leap year.
-  $leap = (($year % 400 == 0) || (($year % 4 == 0) && ($year % 100)) ) ? 1 : 0;
-  $mdays = array( 31, ($leap ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 );
-  # Some boundary checks
-  if($year < $epoch || $year > 9999) return $seconds;
-  if($month < 1     || $month > 12)  return $seconds;
-  if($day < 1       || $day > $mdays[ $month - 1 ]) return $seconds;
-  # Accumulate the number of days since the epoch.
-  $days = $day;    # Add days for current month
-  $days += array_sum( array_slice($mdays, 0, $month-1 ) );    # Add days for past months
-  $days += $range * 365;                      # Add days for past years
-  $days += intval( ( $range ) / 4 );             # Add leapdays
-  $days -= intval( ( $range + $offset ) / 100 ); # Subtract 100 year leapdays
-  $days += intval( ( $range + $offset + $norm ) / 400 );  # Add 400 year leapdays
-  $days -= $leap;                                      # Already counted above
-  # Adjust for Excel erroneously treating 1900 as a leap year.
-  if ($days > 59) { $days++;}
-  return $days + $seconds;
+/**
+ * This method converts datetime to Excel format, thanks to Excel::Writer::XLSX::Worksheet.pm (perl).
+ * Contributed by gibus (https://github.com/gibus).
+ *
+ * @param string $date_input String value of data to be set. Should be in YYYY-MM-DD[ hh:mm:ss] format.
+ *
+ * @return number Value in Excel format, ready to be saved in the cell.
+ */
+private function convertDateTime($date_input) {  
+    $days    = 0;    # Number of days since epoch
+    $seconds = 0;    # Time expressed as fraction of 24h hours in seconds
+    $year=$month=$day=0;
+    $hour=$min  =$sec=0;
+    $date_time = $date_input;
+    if (preg_match("/(\d{4})\-(\d{2})\-(\d{2})/", $date_time, $matches))
+    {
+        list($junk,$year,$month,$day) = $matches;
+    }
+    if (preg_match("/(\d{2}):(\d{2}):(\d{2})/", $date_time, $matches))
+    {
+        list($junk,$hour,$min,$sec) = $matches;
+        $seconds = ( $hour * 60 * 60 + $min * 60 + $sec ) / ( 24 * 60 * 60 );
+    }
+    
+    //using 1900 as epoch, not 1904, ignoring 1904 special case
+    # Special cases for Excel.
+    if ("$year-$month-$day"=='1899-12-31')  return $seconds      ;    # Excel 1900 epoch
+    if ("$year-$month-$day"=='1900-01-00')  return $seconds      ;    # Excel 1900 epoch
+    if ("$year-$month-$day"=='1900-02-29')  return 60 + $seconds ;    # Excel false leapday
+    
+    # We calculate the date by calculating the number of days since the epoch
+    # and adjust for the number of leap days. We calculate the number of leap
+    # days by normalising the year in relation to the epoch. Thus the year 2000
+    # becomes 100 for 4 and 100 year leapdays and 400 for 400 year leapdays.
+    $epoch  = 1900;
+    $offset = 0;
+    $norm   = 300;
+    $range  = $year - $epoch;
+    
+    # Set month days and check for leap year.
+    $leap = (($year % 400 == 0) || (($year % 4 == 0) && ($year % 100)) ) ? 1 : 0;
+    $mdays = array( 31, ($leap ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 );
+    
+    # Some boundary checks
+    if($year < $epoch || $year > 9999) return $seconds;
+    if($month < 1     || $month > 12)  return $seconds;
+    if($day < 1       || $day > $mdays[ $month - 1 ]) return $seconds;
+    
+    # Accumulate the number of days since the epoch.
+    $days = $day;    # Add days for current month
+    $days += array_sum( array_slice($mdays, 0, $month-1 ) );    # Add days for past months
+    $days += $range * 365;                      # Add days for past years
+    $days += intval( ( $range ) / 4 );             # Add leapdays
+    $days -= intval( ( $range + $offset ) / 100 ); # Subtract 100 year leapdays
+    $days += intval( ( $range + $offset + $norm ) / 400 );  # Add 400 year leapdays
+    $days -= $leap;                                      # Already counted above
+    
+    # Adjust for Excel erroneously treating 1900 as a leap year.
+    if ($days > 59) { $days++;}
+    return $days + $seconds;
 }
 
-//THIS FUNCTION IS IN TODO LIST.
+/**
+ * This method formats data for writing and actually writes the data into cell. It doesn't deal with 's' (text) data that stored in sharedStrings.xml.
+ *
+ * @param string $type Data type. 'd' - datetime string, all other - the number format.
+ * @param variant $data Data to be set. Date/time values should be passed as strings like 'YYYY-MM-DD[ hh:mm:ss]'.
+ * @param object $c SimpleXML object that represents <c> element of current sheet*.xml.
+ *
+ * @return nothing.
+ */
 private function formatDataWrite($type, $data, $c){
     
     if (isset($c['t']))
